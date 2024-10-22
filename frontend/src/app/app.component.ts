8df45abbe21b9956c77c76b633ed3a24f7029e96
bsodocean <socajowa3@gmail.com>
2024-10-22 22:46:20 +0200

    Added category filtering, add & remove car from the list + created loading-spinner component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { CarsComponent } from './cars/cars.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,33 @@ import { CarsComponent } from './cars/cars.component';
     RouterLinkActive,
     AboutComponent,
     CarsComponent,
+    LoadingSpinnerComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
+
+  isLoading = signal(false);
+
+  constructor() {
+    effect(() => {
+      const spinner = document.getElementById('loading-spinner');
+      if (spinner) {
+        if (this.isLoading()) {
+          spinner.style.display = 'block';
+        } else {
+          spinner.style.display = 'none';
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.isLoading.set(true);
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 2000);
+  }
 }
--- frontend/src/app/cars/cars.component.ts
+++ frontend/src/app/cars/cars.component.ts
@@ -1,49 +1,114 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { map } from 'rxjs';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
+import { delay, map } from 'rxjs';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 export interface Car {
   id: string;
-  model: string;
-  engine: string;
-  color: string;
+  model?: string;
+  engine?: string;
+  color?: string;
 }
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingSpinnerComponent, ReactiveFormsModule],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.scss',
 })
-export class CarsComponent {
-  carModels: Car[] = [];
+export class CarsComponent implements OnInit {
+  cars: Car[] = [];
   filteredCars: Car[] = [];
+  isLoading = signal(false);
+  noCarsAvailable: boolean = false;
+  showForm: boolean = false;
+  addCarForm: FormGroup;
 
   constructor(private http: HttpClient) {
+    effect(() => {
+      const spinner = document.getElementById('loading-spinner');
+      if (spinner) {
+        spinner.style.display = this.isLoading() ? 'block' : 'none';
+      }
+    });
+
+    this.addCarForm = new FormGroup({
+      model: new FormControl(''),
+      engine: new FormControl(''),
+      color: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {
     this.fetchCars();
   }
 
+  toggleCarForm() {
+    this.showForm = !this.showForm;
+  }
+
   fetchCars() {
+    this.isLoading.set(true);
+
     this.http
       .get<{ data: Car[] }>('http://localhost:5001/api/cars')
-      .pipe(map((res) => res.data))
-      .subscribe((cars) => {
-        this.carModels = cars;
-        this.filteredCars = cars; // Initialize with all cars
+      .pipe(
+        map((res) => res.data),
+        delay(2000)
+      )
+      .subscribe({
+        next: (cars) => {
+          console.log(cars);
+          this.cars = cars;
+          this.filteredCars = cars;
+          this.isLoading.set(false);
+        },
+        error: (error) => {
+          console.error('Error loading cars', error);
+          this.isLoading.set(false);
+        },
       });
   }
 
   filterCars(model?: string) {
     if (model === 'All' || !model) {
-      this.filteredCars = this.carModels;
+      this.filteredCars = this.cars;
+      this.noCarsAvailable = false;
     } else {
-      this.filteredCars = this.carModels.filter((car) => car.model === model);
+      this.filteredCars = this.cars.filter((car) => car.model === model);
+      this.noCarsAvailable = this.filteredCars.length === 0;
     }
+  }
+
+  onSubmit() {
+    if (this.addCarForm.valid) {
+      const carModel = this.addCarForm.get('model')?.value;
+      const carEngine = this.addCarForm.get('engine')?.value;
+      const carColor = this.addCarForm.get('color')?.value;
+
+      this.addCar(carModel, carEngine, carColor);
+      this.addCarForm.reset();
+      this.showForm = false;
+    }
+  }
+
+  addCar(model: string, engine: string, color: string) {
+    let newCar = {
+      id: (this.cars.length + 1).toString(),
+      model: model,
+      engine: engine,
+      color: color,
+    };
+    this.cars.push(newCar);
+    this.http.post('http://localhost:5001/api/cars', newCar).subscribe();
   }
 
   removeCar(index: number) {
     this.filteredCars.splice(index, 1);
+    // this.http.delete(`http://localhost:5001/api/cars:822104945018`).subscribe();
+    // console.log(index);
   }
 }
